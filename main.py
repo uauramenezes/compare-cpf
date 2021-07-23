929c2e76f20671e66433a2cd1e39e0e09126a766
Uaurá Menezes <uaurasilva@hotmail.com>
2021-07-23 14:20:06 -0300

    Generate file with better format

--- main.py
+++ main.py
@@ -7,49 +7,68 @@ import re
 
 
 def find_deceased(files):
-    cpf_obito = []
-    cpf_prev = []
-    prev = []
+    # TODO: Try to return single lists
+    previcon = {
+        'cpf': [],
+        'name': [],
+        'registration': []
+    }
+    deceased = {
+        'cpf': [],
+        'date_of_birth': [],
+        'date_of_death': [],
+    }
 
     def handle_obi_file(path):
         with open(path) as file:
             lines = file.read().splitlines()
             for line in lines:
-                # TODO: Return date of death
-                cpf_obito.append(line[163:174])
+                deceased['cpf'].append(line[163:174])
+                deceased['date_of_birth'].append(line[147:155])
+                deceased['date_of_death'].append(line[155:163])
 
     def handle_csv(path):
         with open(path, newline='') as csvfile:
             lines = csv.reader(csvfile, delimiter=';')
-            index = next(lines).index("CPF")
+            next(lines)
             for row in lines:
                 if row[0] == "*":
                     continue
-                # TODO: Try to return a single lists
-                cpf = row[index].replace('.', '').replace('-', '')
-                cpf_prev.append(cpf)
-                prev.append(row[0:3])
+                cpf = row[2].replace('.', '').replace('-', '')
+                previcon['cpf'].append(cpf)
+                previcon['name'].append(row[0])
+                previcon['registration'].append(row[1])
 
-    def get_cpf_list():
-        cpf = []
-        cpf_list = set(cpf_prev).intersection(cpf_obito)
+    def get_previcon_deceased_list():
+        previcon_deceased_list = []
+        cpf_list = set(previcon['cpf']).intersection(deceased['cpf'])
         for item in cpf_list:
-            index = cpf_prev.index(item)
-            cpf.append(prev[index])
-        return cpf
+            index_previcon = previcon['cpf'].index(item)
+            index_deceased = deceased['cpf'].index(item)
+            prev_deceased = [previcon['cpf'][index_previcon],
+                             previcon['registration'][index_previcon],
+                             deceased['date_of_birth'][index_deceased],
+                             deceased['date_of_death'][index_deceased],
+                             previcon['name'][index_previcon],
+                             ]
+            previcon_deceased_list.append(prev_deceased)
+        return previcon_deceased_list
 
     def create_file():
-        cpf = get_cpf_list()
+        previcon_deceased = get_previcon_deceased_list()
 
         path = files[0].split('/')
         path = '/'.join(path[0:-1])
-        # TODO: Write the columns names
+        header = 'CPF\t\t\tMatrícula\tNascimento\t\tÓbito\t\t\tNome\n'
         with open(f'{path}/obitos.txt', 'w') as file:
-            for item in cpf:
-                file.write(f"\t\t".join(item[::-1]) + '\n')
+            file.write(header)
+            for item in previcon_deceased:
+                file.write(f"\t\t".join(item) + '\n')
 
     for file in files:
         file_format = file.split('.').pop()
+        if len(files) > 3:
+            files.pop()
         if bool(re.search("OBI", file_format)):
             handle_obi_file(file)
         elif file_format == "csv":
