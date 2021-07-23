e9475ed6d6d0c76693c49d75d19e9a99d60a8dfb
Uaurá Menezes <uaurasilva@hotmail.com>
2021-07-23 12:20:19 -0300

    Create GUI class to remove global variable

--- main.py
+++ main.py
@@ -6,19 +6,19 @@ import csv
 import re
 
 
-def search_cpf(files):
+def find_deceased(files):
     cpf_obito = []
     cpf_prev = []
     prev = []
 
-    def open_obi_file(path):
+    def handle_obi_file(path):
         with open(path) as file:
             lines = file.read().splitlines()
             for line in lines:
                 # TODO: Return date of death
                 cpf_obito.append(line[163:174])
 
-    def open_csv(path):
+    def handle_csv(path):
         with open(path, newline='') as csvfile:
             lines = csv.reader(csvfile, delimiter=';')
             index = next(lines).index("CPF")
@@ -30,26 +30,30 @@ def search_cpf(files):
                 cpf_prev.append(cpf)
                 prev.append(row[0:3])
 
-    def create_file():
+    def get_cpf_list():
         cpf = []
         cpf_list = set(cpf_prev).intersection(cpf_obito)
         for item in cpf_list:
             index = cpf_prev.index(item)
             cpf.append(prev[index])
+        return cpf
+
+    def create_file():
+        cpf = get_cpf_list()
 
         path = files[0].split('/')
         path = '/'.join(path[0:-1])
         # TODO: Write the columns names
-        with open(f'{path}/obi/obitos.txt', 'w') as file:
+        with open(f'{path}/obitos.txt', 'w') as file:
             for item in cpf:
                 file.write(f"\t\t".join(item[::-1]) + '\n')
 
     for file in files:
         file_format = file.split('.').pop()
         if bool(re.search("OBI", file_format)):
-            open_obi_file(file)
+            handle_obi_file(file)
         elif file_format == "csv":
-            open_csv(file)
+            handle_csv(file)
         else:
             showinfo(
                 title='Formato de Arquivo não Suportado',
@@ -64,61 +68,59 @@ def search_cpf(files):
     )
 
 
-# TODO: Make a class to remove global variables
-root = tk.Tk()
-root.title('Comparar arquivos')
-root.resizable(False, False)
-root.geometry('450x150')
+class GUI(tk.Tk):
+    def __init__(self, title, size, resize) -> None:
+        tk.Tk.__init__(self)
+        self.files = []
+        self.title(title)
+        self.geometry(size)
+        self.resizable(resize, resize)
 
-files = []
+    def __select_files(self):
+        filetypes = (
+            ('All files', '*.*'),
+            ('csv files', '*.csv'),
+            ('text files', '*.txt'),
+        )
 
+        path = fd.askopenfilenames(
+            title='Open files',
+            initialdir='/',
+            filetypes=filetypes)
 
-def select_files():
-    filetypes = (
-        ('All files', '*.*'),
-        ('csv files', '*.csv'),
-        ('text files', '*.txt'),
-    )
+        if len(path) == 0:
+            return
+        for n in range(0, len(path)):
+            self.files.insert(n, path[n])
+            filename = path[n].split('/').pop()
+            self.create_label(n, filename)
+            if n >= 2:
+                break
 
-    path = fd.askopenfilenames(
-        title='Open files',
-        initialdir='/',
-        filetypes=filetypes)
+    def __search(self):
+        if len(self.files) == 0:
+            return
+        find_deceased(self.files)
 
-    if len(path) == 0:
-        return
-    for n in range(0, len(path)):
-        files.insert(n, path[n])
-        filename = path[n].split('/').pop()
-        create_label(n, filename)
-        if n >= 2:
-            break
+    def create_button(self, column, text, command):
 
+        function = self.__select_files if command == 'open' else self.__search
 
-def create_button(column, text, function):
-    button = ttk.Button(
-        root,
-        text=text,
-        command=function
-    )
+        button = ttk.Button(
+            self,
+            text=text,
+            command=function
+        )
 
-    button.grid(padx=10, pady=5, row=4, column=column)
-    button.place(x=column * 180 + 100, y=111)
+        button.grid(padx=10, pady=5, row=4, column=column)
+        button.place(x=column * 180 + 100, y=111)
 
-
-def create_label(row, text):
-    label = ttk.Label(width=55, text=text)
-    label.grid(padx=10, pady=5, row=row, column=1)
-
-
-def search():
-    if len(files) == 0:
-        return
-
-    search_cpf(files)
+    def create_label(self, row, text):
+        label = ttk.Label(width=55, text=text)
+        label.grid(padx=10, pady=5, row=row, column=1)
 
 
-create_button(0, "Abrir arquivo", select_files)
-create_button(1, "Fazer busca", search)
-
-root.mainloop()
+gui = GUI('Comparar arquivos', '450x150', False)
+gui.create_button(0, 'Abrir arquivo', 'open')
+gui.create_button(1, 'Fazer busca', 'search')
+gui.mainloop()
